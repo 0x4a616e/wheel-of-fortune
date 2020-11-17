a6c696614f6deb0ebbab85711ef0cfb5904deae6
Jan Gassen <jan.gassen@vorwerk.de>
2020-11-17 13:37:52 +0100

    Initial import

--- wheel.js
+++ wheel.js
@@ -1,10 +1,33 @@
+/*    
+MIT License
+
+Copyright (c) 2017 Jeremy Rue
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 var d3 = require("d3");
 
-var green = "#009a3b";
+var primarycolor = "#009a3b";
 
-window.onload = function () {
-    var chart = document.getElementById("chart");
-    var size = Math.min(chart.offsetHeight, chart.offsetWidth);
+function createWheel(data) {
+  var chart = document.getElementById("chart");
+  var size = Math.min(chart.offsetHeight, chart.offsetWidth);
 
   var padding = { top: 40, right: 40, bottom: 80, left: 40 },
     w = size - padding.left - padding.right,
@@ -16,59 +39,10 @@ window.onload = function () {
     oldpick = [],
     color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  //http://bl.ocks.org/jrue/raw/a2aaf36b3c096925ccbf/
-  //http://osric.com/bingo-card-generator/?title=HTML+and+CSS+BINGO!&words=padding%2Cfont-family%2Ccolor%2Cfont-weight%2Cfont-size%2Cbackground-color%2Cnesting%2Cbottom%2Csans-serif%2Cperiod%2Cpound+sign%2C%EF%B9%A4body%EF%B9%A5%2C%EF%B9%A4ul%EF%B9%A5%2C%EF%B9%A4h1%EF%B9%A5%2Cmargin%2C%3C++%3E%2C{+}%2C%EF%B9%A4p%EF%B9%A5%2C%EF%B9%A4!DOCTYPE+html%EF%B9%A5%2C%EF%B9%A4head%EF%B9%A5%2Ccolon%2C%EF%B9%A4style%EF%B9%A5%2C.html%2CHTML%2CCSS%2CJavaScript%2Cborder&freespace=true&freespaceValue=Web+Design+Master&freespaceRandom=false&width=5&height=5&number=35#results
-
-  var data = [
-    { label: "Delmes, Marco", value: 1, question: "Delmes, Marco" },
-    { label: "Bohle, Celia", value: 1, question: "Bohle, Celia" },
-    { label: "Schneider, Dennis", value: 1, question: "Schneider, Dennis" },
-    { label: "Kiliclar, Bora", value: 1, question: "Kiliclar, Bora" },
-    { label: "Hötte, Thomas", value: 1, question: "Hötte, Thomas" },
-    { label: "Bell, Marcus", value: 1, question: "Bell, Marcus" },
-    { label: "Zohren, Stefan", value: 1, question: "Zohren, Stefan" },
-    { label: "Gerlach, Sven", value: 1, question: "Gerlach, Sven" },
-    { label: "Ochmann, David", value: 1, question: "Ochmann, David" },
-    { label: "Thomas, Michael", value: 1, question: "Thomas, Michael" },
-    { label: "Gassen, Jan", value: 1, question: "Gassen, Jan" },
-    { label: "Riemer, Tim", value: 1, question: "Riemer, Tim" },
-    { label: "Kucay, Markus", value: 1, question: "Kucay, Markus" },
-    {
-      label: "Schulz, Dennis (VCO)",
-      value: 1,
-      question: "Schulz, Dennis (VCO)",
-    },
-    { label: "Cruz, Pedro", value: 1, question: "Cruz, Pedro" },
-    { label: "Arends, Michael", value: 1, question: "Arends, Michael" },
-    { label: "Shah, Milan", value: 1, question: "Shah, Milan" },
-    {
-      label: "Freyer-Hirtz, Ulrich",
-      value: 1,
-      question: "Freyer-Hirtz, Ulrich",
-    },
-    { label: "Dyck, Sebastian", value: 1, question: "Dyck, Sebastian" },
-    { label: "Soyke, Holger", value: 1, question: "Soyke, Holger" },
-    { label: "Hu, Xiaopei (VCO)", value: 1, question: "Hu, Xiaopei (VCO)" },
-    { label: "Knaub, Anna", value: 1, question: "Knaub, Anna" },
-    { label: "Akin, Engin", value: 1, question: "Akin, Engin" },
-    { label: "Röhren, Sebastian", value: 1, question: "Röhren, Sebastian" },
-    { label: "Dujardin, Marcel", value: 1, question: "Dujardin, Marcel" },
-    { label: "Abdulla, Faris", value: 1, question: "Abdulla, Faris" },
-    { label: "Maier, Vitali", value: 1, question: "Maier, Vitali" },
-    { label: "Soudani, Aziz", value: 1, question: "Soudani, Aziz" },
-    { label: "Cimen, Mert", value: 1, question: "Cimen, Mert" },
-    { label: "Popov, Dmitry", value: 1, question: "Popov, Dmitry" },
-    { label: "Nieuwpoort, Richel", value: 1, question: "Nieuwpoort, Richel" },
-    { label: "Schaus, Daniel", value: 1, question: "Schaus, Daniel" },
-    { label: "Nia, Arman", value: 1, question: "Nia, Arman" },
-  ];
-
   var pie = d3
     .pie()
     .sort(null)
-    .value(function (d) {
-      return 1;
-    });
+    .value(d => 1);
 
   var arc = d3.arc().innerRadius(0).outerRadius(r);
 
@@ -119,8 +93,9 @@ window.onload = function () {
     })
     .attr("text-anchor", "end")
     .style("font-size", "14px")
+    .style("fill", "#FFFFFF")
     .text(function (d, i) {
-      return data[i].label;
+      return data[i];
     });
 
   container.on("click", spin);
@@ -131,14 +106,14 @@ window.onload = function () {
     .attr(
       "transform",
       "translate(" +
-        (w +  padding.left +  padding.right) +
+        (w + padding.left + padding.right) +
         "," +
         (h / 2 + padding.top) +
         ")"
     )
     .append("path")
     .attr("d", "M-" + r * 0.15 + ",0L0," + r * 0.05 + "L0,-" + r * 0.05 + "Z")
-    .style("fill", green);
+    .style("fill", primarycolor);
 
   //draw spin circle
   container
@@ -146,7 +121,7 @@ window.onload = function () {
     .attr("cx", 0)
     .attr("cy", 0)
     .attr("r", 60)
-    .style("fill", green)
+    .style("fill", primarycolor)
     .style("cursor", "pointer");
 
   //spin text
@@ -158,7 +133,7 @@ window.onload = function () {
     .text("SPIN")
     .style("font-weight", "bold")
     .style("font-size", "30px")
-    .style("text-color", green);
+    .style("fill", "#000000");
 
   function spin(d) {
     container.on("click", null);
@@ -191,14 +166,8 @@ window.onload = function () {
       .duration(3000)
       .attrTween("transform", rotTween)
       .on("end", function () {
-        //mark question as seen
-        //d3.select(".slice:nth-child(" + (picked + 1) + ") path").style("fill", "#FFF");
-
-        //populate question
-        d3.select("#question h1").text(data[picked].question);
-
+        d3.select("#question h1").text(data[picked]);
         oldrotation = rotation;
-
         container.on("click", spin);
       });
   }
@@ -209,4 +178,4 @@ window.onload = function () {
       return "rotate(" + i(t) + ")";
     };
   }
-};
+}
